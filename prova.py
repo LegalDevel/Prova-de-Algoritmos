55cb51cbac34cdddc433ff71ea3777b2ce2b151b
LegalDevel <heliandroat@gmail.com>
2024-07-24 21:32:35 -0300

    first commit

---
+++ prova.py
@@ -0,0 +1,8 @@
+def calcularAreaRetangulo():
+    base = float(input("Digite o valor da base do retângulo em metros: "))
+    altura = float(input("Digite o valor da altura do retângulo em metros: "))
+    area = base * altura
+
+    print(f"O valor da área do retângulo é: {area} m2")
+
+calcularAreaRetangulo()
